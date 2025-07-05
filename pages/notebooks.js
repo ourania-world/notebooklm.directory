0e3d671e9b64eff890784364fc3d0a11ce1d8530
Ourania <dmk@ourania.world>
2025-07-04 21:10:27 -0400

    Updated notebooks.js

---
+++ pages/notebooks.js
@@ -0,0 +1,8 @@
+export default function Notebooks() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '5rem' }}>
+      <h1>📘 NotebookLM Directory</h1>
+      <p>Coming soon: A curated collection of LM notebooks</p>
+    </div>
+  );
+}
