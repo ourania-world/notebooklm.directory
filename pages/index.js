105ab85b5c1b54d170df04eb1ff29af69db4063c
Ourania <dmk@ourania.world>
2025-07-04 20:53:09 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "5rem" }}>
+      <h1>🚀 NotebookLM Directory MVP is Live</h1>
+      <p>Brought to you by Ourania KA LLC</p>
+    </div>
+  );
+}
