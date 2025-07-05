36ae3345c474ee310faad038d0ade01447358e13
Ourania <dmk@ourania.world>
2025-07-05 01:51:03 -0400

    Updated .env.local

--- pages/browse.js
+++ pages/browse.js
@@ -95,8 +95,6 @@ export default function Browse() {
                 </option>
               );
             })}
-              </option>
-            ))}
           </select>
         </div>
         
