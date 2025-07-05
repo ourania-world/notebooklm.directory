6a539890f57ea83a9e7fd60801776a925b883c4c
Ourania <dmk@ourania.world>
2025-07-04 22:10:59 -0400

    Add audio section to home page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "120ca214ca5a576b"; }
+/******/ 		__webpack_require__.h = function() { return "0d4b581e09270ddd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,14 @@ export default function Notebooks() {
     <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '5rem' }}>
       <h1>📘 NotebookLM Directory</h1>
       <p>Coming soon: A curated collection of LM notebooks</p>
+      
+      <section style={{ textAlign: 'center', marginTop: '2rem' }}>
+        <h2>🎧 Listen to the Vision</h2>
+        <audio controls>
+          <source src="/NotebookLM%20Directory%20Overview.wav" type="audio/wav" />
+          Your browser does not support the audio element.
+        </audio>
+      </section>
     </div>
   );
 }
