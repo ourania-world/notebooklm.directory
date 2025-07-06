312813736fc419895f6b27a7b7cb36033a3d30b6
Ourania <dmk@ourania.world>
2025-07-06 08:01:55 -0400

    Added deploy.js

---
+++ deploy.js
@@ -0,0 +1,4 @@
+// Simple script to help with deployment
+console.log('Preparing for deployment to Vercel...');
+console.log('Environment variables are set in vercel.json');
+console.log('Ready to deploy!');
