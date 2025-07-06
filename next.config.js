54ef5439e1e7ec7b6743034fc580dfd63e75dfcb
Ourania <dmk@ourania.world>
2025-07-06 01:58:40 -0400

    Fix Next.js Configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ['micro']
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig;
