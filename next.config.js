69ffb24ff88b3de47b916514501dee596f3bce9a
Ourania <dmk@ourania.world>
2025-07-08 00:44:08 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,13 +5,7 @@ const nextConfig = {
   images: {
     domains: ['images.pexels.com', 'ciwlmdnmnsymiwmschej.supabase.co'],
   },
-  // Required for Stripe webhooks
-  api: {
-    bodyParser: {
-      sizeLimit: '2mb',
-    },
-    externalResolver: true,
-  },
-}
+  // NOTE: 'api' key removed. Configure bodyParser/externalResolver IN each API route file.
+};
 
 module.exports = nextConfig;
